cbee52eaab7db9c23e2ebf57a023b3921aa5d8a5
ajay <ajaykumarkarri739@gmail.com>
2022-01-28 03:14:58 +0000

    added more lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,20 @@ public class HelloWorld
 	public static void main(string[] args)
 	{
 		system.out.println("I want it job");
-		system.out.println("I want it job");
+	        system.out.println("I want it job");
+  		system.out.println("I want it job");
+	        system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+                system.out.println("I want it job");
+
+
 	
 	}
 	
