78120a1c9c70ca73f34dfe78cdb0cc09bbdc052e
ajay <ajaykumarkarri739@gmail.com>
2022-01-28 03:30:02 +0000

    added more lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,19 +2,19 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		system.out.println("I want it job");
-	        system.out.println("I want it job");
-  		system.out.println("I want it job");
-	        system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
-                system.out.println("I want it job");
+		System.out.println("I want it job");
+	        System.out.println("I want it job");
+  		System.out.println("I want it job");
+	        System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
+                System.out.println("I want it job");
 		System.out.println("I want it job");
 		System.out.println("I want it job");
 
